380434ba02eeb6174998816596a0322617870e8a
VarelaChristianJr.A. <christianvarelajr@gmail.com>
2026-05-01 21:16:44 +0800

    refactor: replace @Data with specific Lombok annotations per Task 2.2

--- src/main/java/com/ws101/varela/unay/EcommerceApi/model/Product.java
+++ src/main/java/com/ws101/varela/unay/EcommerceApi/model/Product.java
@@ -1,12 +1,18 @@
 package com.ws101.varela.unay.EcommerceApi.model;
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter              // Para sa getters
+@Setter              // Para sa setters
+@ToString            // Para sa toString() method
+@EqualsAndHashCode   // Para sa equals() at hashCode()
+@AllArgsConstructor  // Constructor with all fields
+@NoArgsConstructor   // Empty constructor
 public class Product {
     private Long id;              // Unique identifier (ID)
     private String name;          // Product name
